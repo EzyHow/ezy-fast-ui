7aaa3986b3c2238fe6182bbbc31a5cc4cf1aa22e
Harinder Singh <EzyHow@users.noreply.github.com>
2024-08-20 10:16:31 +0530

    add test for URL with query parameter

--- tests/SnippetShow.test.tsx
+++ tests/SnippetShow.test.tsx
@@ -1,12 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest';
 
-import { HashRouter, MemoryRouter, Route, Routes } from 'react-router-dom';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import React from 'react';
 import { snippets } from '../src/data/snippets';
 import { animations } from '../src/data/animations';
 import SnippetShow from '../src/pages/snippets/SnippetShow';
+
+
 
 describe('Renders list page correctly', async () => {
 
@@ -59,6 +61,29 @@ describe('Renders list page correctly', async () => {
 
         expect(iframes.length).toBe(1);
         expect(animationHeading).not.toBeNull();
+
+        expect(htmlBox).not.toBeNull();
+        expect(cssBox).not.toBeNull();
+    });
+
+    it('Should render the snippet correctly with query parameters', async () => {
+        // Setup
+        const randomSnippet = snippets[Math.floor(Math.random() * snippets.length)];
+        const { container } = renderWithRouter([`/snippet/${randomSnippet.slug}?test=1`]);
+        // const location = useLocation();
+        // console.log('URL Path => ', location.pathname);
+        const iframes = container.querySelectorAll('iframe');
+
+        const snippetHeading = screen.queryAllByText(randomSnippet.name);
+        const htmlBox = screen.queryByText('HTML');
+        const cssBox = screen.queryByText('CSS');
+
+        // Expectations
+        expect(iframes.length).toBeGreaterThan(0);
+        expect(iframes[0]).not.toBeNull();
+
+        expect(iframes.length).toBe(1);
+        expect(snippetHeading).not.toBeNull();
 
         expect(htmlBox).not.toBeNull();
         expect(cssBox).not.toBeNull();

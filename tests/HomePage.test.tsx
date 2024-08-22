c59a80312448e33612ff735d9aed691d936d62b7
Harinder Singh <EzyHow@users.noreply.github.com>
2024-08-22 12:00:31 +0530

    updated homepage test to use memoryrouter

--- tests/HomePage.test.tsx
+++ tests/HomePage.test.tsx
@@ -1,19 +1,27 @@
 import { render, screen } from '@testing-library/react'
 import { describe, expect, it } from 'vitest';
 
-import { HashRouter } from 'react-router-dom';
+import { HashRouter, MemoryRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import React from 'react';
 import HomePage from '../src/pages/homepage/HomePage';
+import AppRoutes from '../src/AppRoutes';
 
 describe('Renders homepage page correctly', async () => {
+
+    const renderWithRouter = (initialEntries) => {
+        return render(
+            <HelmetProvider>
+                <MemoryRouter initialEntries={initialEntries}>
+                    <AppRoutes />
+                </MemoryRouter>
+            </HelmetProvider>
+        );
+    };
+
     it('Should render the page correctly', async () => {
         // Setup
-        render(<HelmetProvider>
-            <HashRouter>
-                <HomePage />
-            </HashRouter>
-        </HelmetProvider>);
+        renderWithRouter([`/`]);
         const h1 = await screen.queryByText('Ezy Fast UI');
 
         // Expectations
@@ -22,19 +30,16 @@ describe('Renders homepage page correctly', async () => {
 
     it('Should have anchor tags with href to list of snippets', async () => {
         // Setup
-        render(<HelmetProvider>
-            <HashRouter>
-                <HomePage />
-            </HashRouter>
-        </HelmetProvider>);
+        renderWithRouter([`/`]);
+
         const anchorTag1 = await screen.queryByText('Browse Snippets');
         const anchorTag2 = await screen.queryByText('Browse All Snippets');
 
         // Expectations
         expect(anchorTag1).not.toBeNull();
-        expect(anchorTag1).toHaveAttribute('href', '#/list');
+        expect(anchorTag1).toHaveAttribute('href', '/list');
 
         expect(anchorTag2).not.toBeNull();
-        expect(anchorTag2).toHaveAttribute('href', '#/list');
+        expect(anchorTag2).toHaveAttribute('href', '/list');
     });
 });

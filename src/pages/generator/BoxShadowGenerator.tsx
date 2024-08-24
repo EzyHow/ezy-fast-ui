08d9892c269131b0269444cfa686c87483e0c5a0
Harinder Singh <EzyHow@users.noreply.github.com>
2024-08-24 11:05:39 +0530

    fix: decrease padding on mobile screen

--- src/pages/generator/BoxShadowGenerator.tsx
+++ src/pages/generator/BoxShadowGenerator.tsx
@@ -106,7 +106,7 @@ const BoxShadowGenerator: React.FC = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mb-4 px-6 py-10">
         <div>
           <h3 className="text-xl font-bold text-white text-center mb-2">Configuration</h3>
-          <div className="p-10 bg-gray-800 rounded-2xl">
+          <div className="p-4 md:p-10 bg-gray-800 rounded-2xl">
             <div className="flex justify-end mb-4 text-right">
               <label className="inline font-bold">Old Browser Support:</label>
               <input
